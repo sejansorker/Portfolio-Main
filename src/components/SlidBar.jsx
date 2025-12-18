4a2ab45653c47f2ea4e1d71c2db5ccea8fca9f63
sejansorker <sejansorker@gmail.com>
2025-12-18 22:05:12 +0600

    height fixed

--- src/components/SlidBar.jsx
+++ src/components/SlidBar.jsx
@@ -18,20 +18,20 @@ const SlidBar = () => {
       <div className="">
         <Container>
          <div className=" relative">
-           <div className={`${down ? "h-[400px] " : "h-[150px]"} lg:px-0 border-1 border-amber-500  px-5  lg:h-[805px] w-full relative overflow-hidden pt-10 bg-[rgba(27,27,27,0.92)] text-white my-0 lg:my-5 rounded-[30px] duration-300 ease-in-out`}>
+           <div className={`${down ? "h-[400px] " : "h-[150px]"} lg:px-0 border-1 border-amber-500  px-5 lg:h-[805px] w-full relative overflow-hidden pt-10 bg-[rgba(27,27,27,0.92)] text-white my-0 lg:my-5 rounded-[30px] duration-300 ease-in-out`}>
               <div className="h-full  absolute">
                 <Meteors />
               </div>
             <div className="lg:text-center lg:gap-x-0 gap-x-5 flex lg:flex-col lg:w-[280px] w-full lg:mx-auto border-b-1 border-amber-500 lg:pb-7.5 pb-3">
               <div className=" lg:w-[200px] w-[90px] flex justify-center  lg:mx-auto">
-                <img className='lg:w-50 lg:h-50 w-20 h-20  rounded-[50%]' src={SS} alt="" />
+                <img className='lg:w-45 lg:h-45 w-20 h-20  rounded-[50%]' src={SS} alt="" />
               </div>
               <div className="">
                 <h2 className='lg:py-5.5 py-2 text-white lg:text-[30px] text-[18px] font-bold font-main'>Md. Sejan Sorker</h2>
                 <h2 className='text-white lg:text-[16px]  text-[10px] font-normal lg:font-semibold rounded-[10px] mx-auto w-fit font-main lg:px-3.5 px-2 lg:py-2 py-1 bg-amber-500'>Frontend Developer</h2>
               </div>
             </div>
-            <div className=" lg:mx-auto pt-5 lg:pt-7.5">
+            <div className=" lg:mx-auto pt-5 lg:pt-6">
               <div className="flex items-center lg:justify-center">
                 <div className="lg:h-10 lg:w-10 h-6 w-6 rounded-[5px] border-1 border-[#F8D568] flex items-center justify-center ">
                   <MdOutlineEmail className='text-[#F8D568] lg:text-[18px] text-[10px]' />
@@ -71,7 +71,7 @@ const SlidBar = () => {
                 </div>
               </div>
             </div>
-            <div className="lg:mt-5 ">
+            <div className="lg:mt-0 ">
               <div className="flex lg:gap-5 gap-2 items-center lg:justify-center lg:mt-8 mt-5">
                 <Link to="https://www.facebook.com/share/1C7bAwyPQz/">
                 <FaFacebook className='text-[rgba(255,255,255,0.59)]' />
